59445e78ce3f9a7657ccd836e47c3a47e15ebd54
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-08-18 15:40:02 +0600

    navbar completed

---
+++ app/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="fixed bottom-0 w-full bg-gray-100">
+        <div className="py-3 text-sm text-center">
+          <p>All Right Reserved by Muhammad Sarowar</p>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/components/NavbarT.jsx
@@ -0,0 +1,44 @@
+import Logo from "../../public/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="w-full bg-white shadow-sm fixed z-50 top-0">
+        <div className="navbar">
+          <div className="flex-1">
+            <Link href="/">
+              <Image width={250} height={100} src={Logo} alt="Logo" />
+            </Link>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <Link href="/html">HTML</Link>
+              </li>
+              <li>
+                <Link href="/css">CSS</Link>
+              </li>
+              <li>
+                <Link href="/bootstrap">Bootstrap</Link>
+              </li>
+              <li>
+                <Link href="/tailwind">Tailwind CSS</Link>
+              </li>
+              <li>
+                <Link href="/javascript">JavaScript</Link>
+              </li>
+              <li>
+                <Link href="/react">React</Link>
+              </li>
+              <li>
+                <Link href="/nextjs">Next JS</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
